88a27c1a9172f7690593972f0cd72928000de978
Lars Roholm <lars.roholm@edlund.dk>
2020-02-05 10:30:08 +0100

    Add layout for Tada68

---
+++ keyboards/tada68/keymaps/larsroholm/keymap.c
@@ -0,0 +1,65 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BL 0
+#define _FL 1
+
+// ISO keycodes with Danish layout names and ________ for empty keys.
+#define DK_PLUS KC_MINS       // +
+#define DK_TCK KC_EQL         // Fronttick (´)
+#define DK_DASH KC_SLSH       // -
+#define DK_UML KC_RBRC        // Umlaut (¨)
+#define DK_QUT KC_NUHS        // Singlequote (')
+#define DK_AE KC_SCLN     	  // Æ
+#define DK_AA KC_LBRC     	  // Å
+#define DK_OE KC_QUOT    	    // Ø
+#define DK_LT KC_NUBS       	// <
+#define ________ KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap _BL: (Base Layer) Default Layer
+   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+   * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ + │ ´ │ Backsp│Del│
+   * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
+   * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ Å │ ¨ │     │Ins│
+   * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐ Ent├───┤
+   * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Æ │ Ø │ ' │    │Hom│
+   * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┼───┤
+   * │Shft│ < │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ - │ Shift│ ↑ │End│
+   * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+   * │Ctl │Win │Alt │         Space          │Alt│Fn │Ctl│ ← │ ↓ │ → │
+   * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+   */
+  [_BL] = LAYOUT_iso(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    DK_PLUS, DK_TCK,  KC_BSPC, KC_DEL,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    DK_AA,   DK_UML,           KC_INS,  \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    DK_AE,   DK_OE,   DK_QUT,  KC_ENT,  KC_HOME, \
+    KC_LSFT, DK_LT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  DK_DASH, KC_RSFT, KC_UP,   KC_END, \
+    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_FL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+
+  /* Keymap _FL1: Function Layer
+   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+   * │   │F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│       │Psc│
+   * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
+   * │     │   │   │   │   │   │   │   │   │   │   │   │   │     │   │
+   * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    ├───┤
+   * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │   │
+   * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴┬───┼───┤
+   * │    │   │   │   │   │   │   │   │   │   │   │   │      │PgU│   │
+   * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+   * │    │    │    │                        │   │   │   │   │PgD│   │
+   * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+   */
+  [_FL] = LAYOUT_iso(
+    ________, KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  ________,  KC_PSCR,  \
+    ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________,           ________, \
+    ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, \
+    ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, KC_PGUP,  ________, \
+    ________, ________, ________,                     ________,                               ________, ________, ________, ________, KC_PGDN,  ________
+  ),
+
+};
