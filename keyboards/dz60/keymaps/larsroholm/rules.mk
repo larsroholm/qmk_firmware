beff28085a52553485494a63323ae5029daf030b
Lars Roholm <lars.roholm@edlund.dk>
2020-04-27 12:45:08 +0200

    Add DZ60 layout

---
+++ keyboards/dz60/keymaps/larsroholm/rules.mk
@@ -0,0 +1,13 @@
+# Build Options.
+BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration.
+MOUSEKEY_ENABLE = no	# Mouse keys.
+EXTRAKEY_ENABLE = no	# Audio control and System control.
+CONSOLE_ENABLE = no	# Console for debug.
+COMMAND_ENABLE = no    # Commands for debug and configuration.
+SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend.
+NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work.
+BACKLIGHT_ENABLE = no  # Enable per-key backlight LEDs.
+AUDIO_ENABLE = no    # There is no speaker on this PCB.
+RGBLIGHT_ENABLE = no # Enable the RGB underglow LEDs.
+
+LAYOUTS = 60_iso_5x1u_split_rshift
