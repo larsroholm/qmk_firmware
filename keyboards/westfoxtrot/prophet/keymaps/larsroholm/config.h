1e9caf0450afa008a58352c9cb8fb2951b42e855
Lars Roholm <github@larsroholm.dk>
2021-03-19 21:49:09 +0100

    Set polling rate to 1000 Hz

--- keyboards/westfoxtrot/prophet/keymaps/larsroholm/config.h
+++ keyboards/westfoxtrot/prophet/keymaps/larsroholm/config.h
@@ -1,3 +1,4 @@
 #define TAPPING_TERM 200 // How long before a tap becomes a hold, if set above 500, a key tapped during the tapping term will turn it into a hold too.
 #define PERMISSIVE_HOLD // Makes tap and hold keys trigger the hold if another key is pressed before releasing, even if it hasn't hit the TAPPING_TERM.
 #define TAPPING_FORCE_HOLD // Makes it possible to use a dual role key as modifier shortly after having been tapped. Breaks any Tap Toggle functionality (TT or the One Shot Tap Toggle).
+#define USB_POLLING_INTERVAL_MS 1 // 1000 Hz polling rate.
